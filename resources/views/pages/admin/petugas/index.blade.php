f3286e60381f56cd7b02b7674767836c86d061eb
athmmrr <mathaammarnur09@gmail.com>
2024-07-05 11:03:44 +0700

    BKC 5

--- resources/views/pages/admin/petugas/index.blade.php
+++ resources/views/pages/admin/petugas/index.blade.php
@@ -37,6 +37,7 @@ Data Petugas
               <th class="px-4 py-3">No. Hp</th>
               <th class="px-4 py-3">Email</th>
               <th class="px-4 py-3">Role</th>
+              <th class="px-4 py-3">Aksi</th>
             </tr>
           </thead>
           <tbody class="bg-white divide-y dark:divide-gray-700 dark:bg-gray-800">
@@ -56,6 +57,23 @@ Data Petugas
               </td>
               <td class="px-4 py-3 text-sm">
                 {{ $petugas->roles }}
+              </td>
+              <td>
+                <div class="flex items-center space-x-4 text-sm">
+                  <form action="{{ route('pengaduans.destroy', $petugas->id)}}" method="POST">
+                      @csrf
+                      @method('delete')
+                      <button
+                        class="flex items-center justify-between  text-sm font-medium leading-5 text-red-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray"
+                        aria-label="Delete">
+                        <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
+                          <path fill-rule="evenodd"
+                            d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                            clip-rule="evenodd"></path>
+                        </svg>
+                      </button> 
+                  </form>
+                </div>
               </td>
             </tr>
             @empty
